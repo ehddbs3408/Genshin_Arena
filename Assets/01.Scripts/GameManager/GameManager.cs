5544915c2e039131599e0574316be334c3dd5975
SonSeongBin <thstjdqls2625@gmail.com>
2022-12-22 00:19:10 +0900

    내 지난 날들은 눈 뜨면 잊는 꿈 하잎 보이 너만 원해~ 하잎 보이 내가 전해

--- Assets/01.Scripts/GameManager/GameManager.cs
+++ Assets/01.Scripts/GameManager/GameManager.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject PausePanel;
-    
-    
+    [SerializeField] private GameObject PausePanel;
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            Time.timeScale = 0;
             PausePanel.SetActive(true);
+            Time.timeScale = 0;
+            
         }
     }
 
