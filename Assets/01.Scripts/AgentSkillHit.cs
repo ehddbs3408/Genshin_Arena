a13d148ba2dac9081bc25222eb03e53feb00875e
SonSeongBin <thstjdqls2625@gmail.com>
2022-12-22 19:29:16 +0900

    스킬 다 만듦

--- Assets/01.Scripts/AgentSkillHit.cs
+++ Assets/01.Scripts/AgentSkillHit.cs
@@ -8,7 +8,9 @@ public class AgentSkillHit : MonoBehaviour
 
     private float activeDamage = 0;
 
-    private float Damage = 0;
+    private float Damage = 0f;
+
+    private float totalDamage = 0f;
 
     PlayerJsonData data;
 
@@ -27,6 +29,26 @@ public class AgentSkillHit : MonoBehaviour
             if (DamagePopupPrefab != null)
             {
                 Skill();
+            }
+        }
+
+        else if(other.gameObject.CompareTag("SpecialSkill"))
+        {
+            if(DamagePopupPrefab != null)
+            {
+                if(data.weaponStat.id == 4)
+                {
+                    SkillDamage(1000f);
+                    StartCoroutine(ThreeUP());
+                    totalDamage = Damage * 3;
+
+                }
+                if(data.weaponStat.id == 9)
+                {
+                    SkillDamage(600f);
+                    StartCoroutine(FiveUP());
+                    totalDamage = Damage * 5;
+                }
             }
         }
     }
@@ -50,42 +72,52 @@ public class AgentSkillHit : MonoBehaviour
             case 0:
                 SkillDamage(120f);
                 StartCoroutine(TwoUP());
+                totalDamage = Damage * 2;
                 break;
             case 1:
                 SkillDamage(160f);
                 StartCoroutine(ThreeUP());
+                totalDamage = Damage * 3;
                 break;
             case 2:
                 SkillDamage(210f);
                 StartCoroutine(ThreeUP());
+                totalDamage = Damage * 3;
                 break;
             case 3:
                 SkillDamage(250f);
                 StartCoroutine(FourUP());
+                totalDamage = Damage * 4;
                 break;
             case 4:
                 SkillDamage(300f);
                 StartCoroutine(FiveUP());
+                totalDamage = Damage * 5;
                 break;
             case 5:
                 SkillDamage(70f);
                 StartCoroutine(ThreeUP());
+                totalDamage = Damage * 3;
                 break;
             case 6:
                 SkillDamage(110f);
                 StartCoroutine(FourUP());
+                totalDamage = Damage * 4;
                 break;
             case 7:
                 SkillDamage(160f);
                 StartCoroutine(FourUP());
+                totalDamage = Damage * 4;
                 break;
             case 8:
                 SkillDamage(160f);
                 StartCoroutine(SixUP());
+                totalDamage = Damage * 6;
                 break;
             case 9:
                 SkillDamage(210f);
                 StartCoroutine(SevenUP());
+                totalDamage = Damage * 7;
                 break;
 
         }
